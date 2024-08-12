c9a9832a132829e727f3da85aac814ee26c93661
Rodrigo Dorat <rdoratm@gmail.com>
2024-08-12 15:11:02 -0400

    :sparkles: Finish auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,24 +1,43 @@
-import { Controller, Inject, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { observableErrorHandler } from 'src/common';
 import { NATS_SERVICE } from 'src/config';
 import { AuthPatterns } from './enums';
+import { LoginUserDto, RegisterUserDto } from './dto';
+import { AuthGuard } from './guards';
+import { Token, User } from './decorators';
+import { ICurrentUser } from './interfaces';
 
 @Controller('auth')
 export class AuthController {
   constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   @Post('register')
-  registerUser() {
-    return this.client.send(AuthPatterns.register, {});
+  registerUser(@Body() registerUserDto: RegisterUserDto) {
+    return observableErrorHandler(
+      this.client.send(AuthPatterns.register, registerUserDto),
+    );
   }
 
   @Put('login')
-  login() {
-    return this.client.send(AuthPatterns.login, {});
+  loginUser(@Body() loginUserDto: LoginUserDto) {
+    return observableErrorHandler(
+      this.client.send(AuthPatterns.login, loginUserDto),
+    );
   }
 
-  @Put('verify')
-  verifyToken() {
-    return this.client.send(AuthPatterns.verify, {});
+  @UseGuards(AuthGuard)
+  @Get('verify')
+  verifyToken(@User() user: ICurrentUser, @Token() token: string) {
+    // return observableErrorHandler(this.client.send(AuthPatterns.verify, {}));
+    return { user, token };
   }
 }
